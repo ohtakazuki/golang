1c2ae9cf2a9d0fbbe30233111db08cc015244c9a
kazuki_ota <ohtakazuki@gmail.com>
2024-03-27 09:03:29 +0900

    レッスン2前半完了

--- lesson02/01_basic/basic.go
+++ lesson02/01_basic/basic.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("ようこそ Go言語へ！")
+	fmt.Println("学習を始めましょう！")
 }
---
+++ lesson02/02_variable/variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var number1 int = 1
+    fmt.Println(number1)
+
+    str1 := "abc"
+    fmt.Println(str1)
+
+    // var unused string
+}
